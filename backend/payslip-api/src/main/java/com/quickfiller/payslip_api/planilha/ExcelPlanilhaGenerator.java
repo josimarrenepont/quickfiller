014265aabac4449ea037be7a9fdd776798842539
josimarrenepont <jnrenepont@hotmail.com>
2026-08-13 01:02:31 -0300

    feat: create ExcelPlanilhaGenerator

---
+++ backend/payslip-api/src/main/java/com/quickfiller/payslip_api/planilha/ExcelPlanilhaGenerator.java
@@ -0,0 +1,18 @@
+package com.quickfiller.payslip_api.planilha;
+
+import com.quickfiller.payslip_api.core.port.PlanilhaGeneratorPort;
+
+public class ExcelPlanilhaGenerator implements PlanilhaGeneratorPort {
+    
+    private byte[] bytes;
+
+    @Override
+    public byte[] gerar(Object data, String formato) throws Exception {
+        return bytes;
+    }
+
+    @Override
+    public String getFormato() {
+        return "xlsx";
+    }
+}
